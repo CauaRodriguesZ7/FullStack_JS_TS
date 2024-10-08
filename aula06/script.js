c479fdc9e8c133a88da96fc84e991ae2f861440a
Caua Aquiles <rodrigzs.soare@gmail.com>
2024-10-08 16:12:37 -0300

    aula 07

---
+++ aula06/script.js
@@ -0,0 +1,7 @@
+let nome = "Fabio" //String
+
+console.log(nome, 'nasceu em 1984. ');
+console.log('Em 2000', nome, 'conhceu Maria. ');
+console.log(nome, 'casou-se com Maria em 2012. ');
+console.log('Maria teve um 1 filho com', nome, 'em 2015. ');
+console.log('O filho de', nome, 'se chama Eduardo. ');
