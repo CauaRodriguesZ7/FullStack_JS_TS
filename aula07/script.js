513388dfed587f168dfe9327ee5eddedfdb2e9b8
Caua Aquiles <rodrigzs.soare@gmail.com>
2024-10-08 16:38:25 -0300

    constantes aula 07

---
+++ aula07/script.js
@@ -0,0 +1,9 @@
+// Nao utilize var, utilize CONST.
+// Constantes NAO podem ser alteradas ao decorrer do codigo
+
+const primeiroNumero = '5'; // 510
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+console.log(typeof (primeiroNumero + segundoNumero))
