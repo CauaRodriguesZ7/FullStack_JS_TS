25d645301fb74c1709933b3dfec11ff321c8c214
Caua Aquiles <rodrigzs.soare@gmail.com>
2024-10-08 17:41:27 -0300

    aula 09 tipos de dados primitivos

---
+++ aula09/script.js
@@ -0,0 +1,8 @@
+const nome1 = 'Caua'; // String com aspas simples
+const nome2 = "Carlos"; // String com aspas duplas
+const nome3 = `Thomas`; // String com crase
+const num1 = 10; // number
+const num2 = 10.52; // number com ponto flutuante
+let nomeAluno; // undefined -> não aponta pra local nenhum na memória
+const sobrenomeAluno = null; // nulo -> não aponta pra local nenhum na memória 
+const boolean = true; // Boolean = true, false (lógico) 
