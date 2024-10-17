7da54b4a60a66618442f12c753f886e183562971
Caua Rodrigues <rodrigzs.soare@gmail.com>
2024-10-17 15:29:57 -0300

    aula 16 - arrays

---
+++ aula16/arrays.js
@@ -0,0 +1,17 @@
+//               0       1        2
+const alunos = ['Luiz', 'Maria', 'Thomas'];
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array); // Verifica se é um array ou nao
+
+// alunos.shift(); // apaga o primeiro elemento do array 
+// alunos.pop(); // apaga ultimo elemento do array
+
+//   alunos.unshift('Luiza'); // Adiciona no começo do array
+// console.log(alunos);
+
+// alunos.push('Luiza'); // Adiciona no fim do array
+// console.log(alunos);
+
+// alunos[0] = 'Cauã';
+// alunos[3] = 'João';
