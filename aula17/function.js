820748a5854bd3765fd36d2afab17ba2390380ec
Caua Rodrigues <rodrigzs.soare@gmail.com>
2024-10-17 15:59:52 -0300

    aula 17 - function basico

---
+++ aula17/function.js
@@ -0,0 +1,24 @@
+function soma(x = 'Cauã', y = ' Aquiles') { // declara funcao e determina um valor padrao aos atributos criados
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma();
+console.log(resultado);
+
+
+// --- FUNCAO MAIS COMPLEXA ---
+// const raiz = function (n) {
+//     return n ** 0.5;
+// };
+
+// console.log(raiz(30));
+// console.log(raiz(20));
+// console.log(raiz(10));
+
+// --- FUNCAO MAIS SIMPLES ---
+// const raiz = n => n ** 0.5;
+
+// console.log(raiz(30));
+// console.log(raiz(20));
+// console.log(raiz(10));
